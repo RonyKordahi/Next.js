4cedfd185f0ef819bf394f58e2559507ad311dd5
Rony <kordrony@gmail.com>
2024-09-12 11:04:18 -0400

    push

---
+++ 08-optimization/app/feed/page.js
@@ -0,0 +1,31 @@
+import Posts from '@/components/posts';
+import { getPosts } from '@/lib/posts';
+
+// // Static metadata
+// export const metadata = {
+//     title: "All Posts",
+//     description: "Browse all our posts.",
+// }
+
+// Dynamic metadat
+export const generateMetadata = async () => {
+    const posts = await getPosts();
+    const numberOfPosts = posts.length;
+
+    return {
+        title: `Browse all our ${numberOfPosts} posts.`,
+        description: "Browse all our posts."
+    }
+}
+
+export default async function FeedPage() {
+
+    const posts = await getPosts();
+
+    return (
+        <>
+            <h1>All posts by all users</h1>
+            <Posts posts={posts} />
+        </>
+    );
+}
